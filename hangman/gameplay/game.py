db411e48772aecf77075bc0f491e92024c0ad814
Oskars <oskars.vanags@outlook.com>
2021-10-18 21:04:14 +0300

    18.10.2021

--- hangman/gameplay/game.py
+++ hangman/gameplay/game.py
@@ -11,57 +11,73 @@ class Game:
         self.progress = list(self.progress)
         self.text_progress = ''.join(self.progress)
         self.text = "Tev vēl ir {} dzīvības, progress: {}"
+        self.letter = ''
 
     def play(self):
-        self.word_length()
+      #  self.word_length()
         while self.lives > 0 and self.flag == False:
             print("\n")
             print(self.text.format(self.lives, self.text_progress))
-            letter = input("Ievadiet minamo burtu: ").upper()
+            self.letter = input("Ievadiet minamo burtu: ").upper()
 
-            if len(letter) == 1 and letter.isalpha():
+            if self.letter in self.try_letters:
+                print(f"Šādu burtu {self.letter} Tu jau minēji!" + " Esi uzmanīgāks")
+            
+            elif len(self.letter) == 1 and self.letter.isalpha():
                 print("Burts atbilst prasībām!")
-                if letter in self.word:
+                if self.letter in self.word:
                     print("Burts atrodas minētajā vārdā!")
-                    self.try_letters.append(letter)
+                    self.try_letters.append(self.letter)
+                    print("Minētie burti: " + str(self.try_letters))
                     for index, burts in enumerate (self.word):
-                        if burts == letter:
-                            self.progress[index] = letter
+                        if burts == self.letter:
+                            self.progress[index] = self.letter
                             self.text_progress = ''.join(self.progress)
                         if "".join(self.progress) == self.word:
-                           print("Vārds ir uzminēts!")
-                           print(self.word)
-                           self.flag = True
-                           break
+                            print("Vārds ir uzminēts!")
+                            self.try_words.append(self.word)
+                            #print(self.word)
+                            self.flag = True
+                            break
                 else:
-                    print("Burts neatrodas minētajā vārdā " + self.text_progress)
+                    
+                    print("Burts neatrodas minētajā vārdā")
                     self.lives = self.lives -1
-                    self.try_letters.append(letter)
+                    self.try_letters.append(self.letter)
+                    print("Minētie burti: " + str(self.try_letters))
                     if self.lives == 0:
                         print("Vārds nav uzminēts, spēle zaudēta!")
 
-            elif len(letter) == len(self.word) and letter.isalpha():
-                if letter == self.word:
+            elif len(self.letter) == len(self.word) and self.letter.isalpha():
+                if self.letter == self.word:
                     print("Vārds ir uzminēts!")
-                    print(self.word)
+                    self.try_words.append(self.word)
+                    #print(self.word)
                     self.flag = True
                     break
-                elif letter != self.word:
-                    for index, find_letter in enumerate (letter):
+
+                elif self.letter != self.word:
+                    for index, find_letter in enumerate (self.letter):
                         if self.word.find(find_letter) != -1:
                             print("Vārds nav uzminēts, bet daži burti sakrīt")
                             break
-                elif letter != self.word:
+                elif self.letter != self.word:
                     print("Ievadītais vārds nesakrīt ar minamo vārdu!")
                     self.lives = self.lives -1
 
             else:
                 print("Burts neatbilst prasībām!")
 
-    def word_length(self):
-        while len(self.word) < 6:
-            print("Ievadītais vārds ir pārāk īss!")
-            self.word = getpass.getpass('Ievadiet minamo vārdu: ').upper()
-            self.progress = "-" * len(self.word)
-            self.progress = list(self.progress)
-            self.text_progress = ''.join(self.progress)
+
+ #   def word_word(self):
+  #      if len(self.try_words) > 0:
+   #         print (self.try_words)
+
+
+#   def word_length(self):
+#        while len(self.word) < 6:
+ #           print("Ievadītais vārds ir pārāk īss!")
+ #           self.word = getpass.getpass('Ievadiet minamo vārdu: ').upper()
+ #           self.progress = "-" * len(self.word)
+ #           self.progress = list(self.progress)
+  #          self.text_progress = ''.join(self.progress)
--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,5 +1,22 @@
 from gameplay.game import Game
 import getpass
+import os
+import random
 
-game1 = Game(getpass.getpass('Ievadiet minamo vārdu: '))
-game1.play()
+with open('data/words.txt', 'r', encoding='utf-8') as file:
+    lines = file.read()
+    words = lines.split('\n')
+
+random.shuffle(words)
+guessed_words = list()
+
+while True:
+    word = words.pop()
+    game = Game(word)
+    game.play()
+
+    guessed_words.append(''.join(game.try_words))
+    print(f"Guessed words: {', '.join(guessed_words)}")
+
+    print('\n' + "_"*70)
+
