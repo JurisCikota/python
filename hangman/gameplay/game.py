a830c0698f3254384626f00af39fc46351bd86c3
Oskars <oskars.vanags@outlook.com>
2021-11-06 14:42:18 +0200

    Merge branch 'master' of https://github.com/JurisCikota/python

--- hangman/gameplay/game.py
+++ hangman/gameplay/game.py
@@ -1,6 +1,6 @@
 import os
 from scripts.word_weight import Score
-
+from scripts.timer import timer
 # TODO
 # izvadīt zīmējumu karātavas! (ar progressu dzīvības)
 # pulkstenis
@@ -32,8 +32,10 @@ class Game:
         while self.lives > 0 and self.flag == False:
             print('\n' + "_"*70 + "\n")
             print(self.text.format(self.lives, self.text_progress))
+            laiks = timer()
+            laiks.time()
             self.letter = input("Ievadiet minamo burtu: ").upper()
-
+            
             if self.letter in self.try_letters:
                 print(f"Šādu burtu {self.letter} Tu jau minēji!" + " Esi uzmanīgāks")
             
